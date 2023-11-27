a650535ae96e0c1a54edb5fc6908cb7b26eedcc1
saranedubridge <saranraj.1803164@srec.ac.in>
2023-11-27 10:18:35 +0530

    end point all completed

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express= require('express')
 const app = express();
 
-let note =[
+//middleware
+app.use(express.json());
+
+let notes =[
 {
     id:1,
     content:'backend server using nodejs',
@@ -15,20 +18,105 @@ let note =[
 
 },
 {
-    id:2,
+    id:3,
     content:'backend restful using nodejs',
-    important: false
+    important: true
 
 }
 
 
 ];
 
+// set endpoint 
+// set the / route
 
+app.get('/',(request,response)=>{
+
+    response.send('<h1>Hello world</h1>')
+});
+
+//to get all the notes
+
+app.get ('/api/notes',(request,response)=>{
+    response.status(200).json(notes);
+})
+
+
+// Create a new resources based on the request
+
+app.post('/api/notes',(request,response)=>{
+    notes = notes.concat(request.body)
+    response.status(201).json({message:"Note created sucessfully "})
+})
+
+
+
+// fetches a single resources based on id
+app.get('/api/notes/:id',(request,response)=>{
+    const  id = request.params.id;
+    const note=notes.find(note=>note.id==id);
+    if(note){
+        response.status(200).json(note);
+    }
+    else{
+        response.status(404).json({message:"id does not exists"})
+    }
+   
+})
+
+
+// delete a single resources based on id
+
+app.delete('/api/notes/:id',(request,response)=>{
+    // get the id
+    const id =request.params.id
+    const note=notes.find(note=>note.id==id);
+notes= notes.filter(note=>note.id !=id);
+if(note){
+    response.status(204).json(note);
+ 
+}else{
+    response.status(404).json({message:'id does not exists '})
+}
+
+});
+
+// put request=> Replaces the entire notes identifed by api
+
+app.put('/api/notes/:id',(request,response)=>{
+    const id = request.params.id;
+    const noteToReplace=request.body;
+
+    notes=notes.map(note=>note.id==id?noteToReplace:note);
+    const note=notes.find(note=>note.id==id);
+    if(note){
+        response.status(200).json({message:'note Replaced'})
+    }else{
+        response.status(404).json({message:'Id does not exist'})
+    }
+});
+
+
+// patch request=> update or replace a single field  identifed by api
+
+app.patch('/api/notes/:id',(request,response)=>{
+    const id = request.params.id;
+    const noteToReplace=request.body;
+const note =notes.find(note=>note.id==id)
+  notes=  notes.map(note=>note.id==id?{...note,...noteToReplace}:note);
+    if(note){
+        response.status(200).json({message:"note patched"});
+
+    }else{
+        response.status(404).json({message:"id does not exist"});
+    }
+});
 
 const PORT =3001;
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 
 });
+
+
 
